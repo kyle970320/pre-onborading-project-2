efbf81ca2be05792a1f12ccfb58046fd3dca3086
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-10-29 17:42:44 +0900

    Add: components 폴더에 공통 Header 컴포넌트, style폴더에 style.module.css파일 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import IssueList from './pages/IssueList/IssueList';
 import IssueListDetail from './pages/IssueListDetail/IssueListDetail';
 
 const Router = () => {
   return (
     <>
+    <Header/>
       <Routes>
         <Route path='/' element={<IssueList/>}></Route>
         <Route path='/detail' element={<IssueListDetail/>}></Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../style/style.module.css'
+
+const Header = () => {
+  return (
+    <header className={style.head}>
+      헤더부분
+    </header>
+  );
+};
+
+export default Header;
