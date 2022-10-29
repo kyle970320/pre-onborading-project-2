556d3f14b4d5098204394d03083549053468976b
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-10-29 17:59:37 +0900

    Feat: context 생성, suspense로 로딩화면 구상중

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import IssueList from './pages/IssueList/IssueList';
 import IssueListDetail from './pages/IssueListDetail/IssueListDetail';
 
+const Context = React.createContext()
 const Router = () => {
   return (
-    <>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<IssueList/>}></Route>
-        <Route path='/detail' element={<IssueListDetail/>}></Route>
-      </Routes>
-    </>
+    <Context.Provider>
+    {/* api 연동 후 suspense 작동 확인 요망 */}
+      <Suspense fallback={<h1>로딩중</h1>}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<IssueList />}></Route>
+          <Route path='/detail' element={<IssueListDetail />}></Route>
+        </Routes>
+      </Suspense>
+    </Context.Provider>
   );
 };
 
